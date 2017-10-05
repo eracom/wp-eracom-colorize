1dd8e69d1ce0a16dc7628d9bf671bd35b9fd161c
Manuel Schmalstieg <git.code@ms-studio.net>
2017-10-05 08:31:38 +0200

    correction des comparateurs

--- colorize.php
+++ colorize.php
@@ -12,48 +12,47 @@ Author URI: https://github.com/ms-studio
 function eracom_colorize() {
 
 	// Obtenir la date courante
-	
+
 	$mois = date('n'); // numéro du mois de 1 à 12
 	$semaine = date('W'); // numéro de semaine (format ISO)
-	
+
 	// 1) Changement de couleur selon les saisons:
-	
-	if ( ( $mois > 3 ) || ( $mois == 12 ) ) { // hiver
-	
-		$bg = 'rgb(220, 220, 220)';
-		
-	} else if ( $mois > 6 ) { // printemps
-	
-		$bg = 'rgb(220, 220, 176)';
-	
-	} else if ( $mois > 9 ) { // été
-	
-		$bg = 'rgb(220, 220, 176)';
-	
-	} else if ( $mois > 12 ) { // automne
-	
-		$bg = 'rgb(220, 220, 176)';
-	
+
+	if ( ( $mois < 3 || $mois == 12 ) ) { // hiver
+
+		$bg = 'SNOW';
+
+	} else if ( $mois < 6 ) { // printemps
+
+		$bg = 'LIGHTGREEN';
+
+	} else if ( $mois < 9 ) { // été
+
+		$bg = 'GOLD';
+
+	} else if ( $mois < 12 ) { // automne
+
+		$bg = 'INDIANRED';
+
 	}
-	
+
 	// 2) Couleurs pour semaines spéciales:
-	
+
 	if ( $semaine == 43 ) { // semaine halloween
-	
-		$bg = 'rgb(220, 176, 176)';
-	
+
+		$bg = 'DARKORANGE';
+
 	}
-	
+
 	// Appliquer la couleur:
   echo '<style>
-  
+	/* WP Eracom Colorize */
 	body #page-container .et_slide_in_menu_container  {
 		background-color: '.$bg.';
 	}
-  	
+
 </style>
 ';
 }
 
 add_action('wp_head','eracom_colorize', 25);
-
